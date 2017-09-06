891a7fc9883dd61b402832984dacf49e0293e1d0
Tan Jian Chern <jctan93@jctan93nuc>
2017-09-06 09:47:19 +0800

    update test.sh

--- test.sh
+++ test.sh
@@ -8,6 +8,12 @@
 #cat test.txt
 #cd $abc
 
-HELLOWORLD= 'echo  "HELLOWORLD" > test.txt'
+echo  "HELLOWORLD" > test.txt
+HELLOWORLD='echo $?'
 if [ $HELLOWORLD -eq 0 ]; then
-	lava-test-case helloworld --result pass
+	lava-test-case helloworld-create --result pass
+fi
+HELLOWORLDCAT='echo test.txt'
+if [ $HELLOWORLDCAT == 'HELLOWORLD' ]; then
+	lava-test-case helloworld-cat --result pass
+fi
