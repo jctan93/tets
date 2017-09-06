69e9e3fd6e78fcf9a94ca6e2a28f1841d569fe7b
Tan Jian Chern <jctan93@jctan93nuc>
2017-09-06 10:20:34 +0800

    test.sh upd

--- test.sh
+++ test.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-#ls -la /
-#mkdir test
-#abc=$(pwd)
-#cd test
-#echo 'HELLOWORLD' > test.txt
-#cat test.txt
-#cd $abc
-
 echo "HELLOWORLD" > test.txt
 if [ $? -eq 0 ]; then
 	lava-test-case helloworld-create --result pass
+else
+	lava-test-case helloworld-create --result fail
 fi
+
 helloworldcat=$(cat test.txt)
 if [ "$helloworldcat" == 'HELLOWORLD' ]; then
 	lava-test-case helloworld-cat --result pass
+else
+	lava-test-case helloworld-cat --result fail
 fi
