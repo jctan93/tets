4abb99bd27d816421910740ea380c7f18d1ddd61
Tan Jian Chern <jctan93@jctan93nuc>
2017-09-06 09:28:21 +0800

    test.sh update

--- test.sh
+++ test.sh
@@ -4,7 +4,10 @@
 #mkdir test
 #abc=$(pwd)
 #cd test
-echo 'HELLOWORLD' > test.txt
-cat test.txt
+#echo 'HELLOWORLD' > test.txt
+#cat test.txt
 #cd $abc
 
+HELLOWORLD= 'echo  "HELLOWORLD" > test.txt'
+if [ $HELLOWORLD -eq 0 ]; then
+	lava-test-case helloworld --result pass
